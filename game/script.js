f6b72322a7c665ff5b65c1d54e308e3680f4ebbe
ArteShow <sokartemax@gmail.com>
2026-05-26 13:06:25 +0200

    add background image

--- game/script.js
+++ game/script.js
@@ -18,11 +18,13 @@ let config = {
 }
 
 function preload() {
-
+    this.load.image("clouds", "./images/clouds.jpg")
+    this.load.image("bullet", "./images/bullet.png")
 }
 
 function create() {
-    
+    this.add.tileSprite(0, 0, 800, 600, "clouds").setOrigin(0, 0)
+    this.add.tileSprite(400, 300, 30, 30, "bullet")
 }
 
 function update() {
